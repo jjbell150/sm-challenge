984451f68aead28a86392331521c59aeb5d614b4
Jesse Bell <jjbell150@gmail.com>
2023-10-01 20:15:23 +1100

    Refactor unit tests

--- src/server.spec.js
+++ src/server.spec.js
@@ -1,45 +1,89 @@
 const axios = require('axios');
 
-// Create test
-describe('POST /issues', () => {
-  it('should create a new issue', async () => {
-    const issue = {
-      title: 'Bug',
-      description: 'This is a bug.',
-    };
-    const response = await axios.post('http://localhost:3000/issues', issue);
-    expect(response.status).toBe(201);
-    expect(response.data).toEqual(expect.objectContaining(issue));
-  });
-});
+describe('Issue API', () => {
+    const baseUrl = 'http://localhost:3000';
 
-// Read test
-describe('GET /issues', () => {
-  it('should get all issues', async () => {
-    const response = await axios.get('http://localhost:3000/issues');
-    expect(response.status).toBe(200);
-    expect(response.data).toEqual(expect.arrayContaining([]));
-  });
-});
+    // GET /issues/:id
+    describe('GET /issues/:id', () => {
+        it('should return the issue with the specified id', async () => {
+            const response = await axios.get(`${baseUrl}/issues/1`);
+            expect(response.status).toBe(200);
+            expect(response.data).toEqual({
+                id: 1,
+                title: 'Issue 1',
+                description: 'This is issue 1'
+            });
+        });
 
-// Update test
-describe('PUT /issues/:id', () => {
-  it('should update an existing issue', async () => {
-    const updatedIssue = {
-      title: 'Bug',
-      description: 'This is an updated bug.',
-    };
-    const response = await axios.put('http://localhost:3000/issues/1', updatedIssue);
-    expect(response.status).toBe(200);
-    expect(response.data).toEqual(expect.objectContaining(updatedIssue));
-  });
-});
+        it('should return 404 when the issue with the specified id does not exist', async () => {
+            try {
+                await axios.get(`${baseUrl}/issues/999`);
+            } catch (error) {
+                expect(error.response.status).toBe(404);
+                expect(error.response.data).toEqual({ message: 'Issue not found' });
+            }
+        });
+    });
 
-// Delete test
-describe('DELETE /issues/:id', () => {
-  it('should delete an issue by ID', async () => {
-    const response = await axios.delete('http://localhost:3000/issues/1');
-    expect(response.status).toBe(200);
-    expect(response.data).toEqual(expect.stringContaining('Deleted issue'));
-  });
+    // POST /issues
+    describe('POST /issues', () => {
+        it('should create a new issue and return it', async () => {
+            const newIssue = {
+                title: 'New Issue',
+                description: 'This is a new issue'
+            };
+
+            const response = await axios.post(`${baseUrl}/issues`, newIssue);
+            expect(response.status).toBe(201);
+            expect(response.data).toEqual({
+                id: 3,
+                title: 'New Issue',
+                description: 'This is a new issue'
+            });
+        });
+    });
+
+    // PUT /issues/:id
+    describe('PUT /issues/:id', () => {
+        it('should update the issue with the specified id and return it', async () => {
+            const updatedIssue = {
+                title: 'Updated Issue',
+                description: 'This is the updated issue'
+            };
+
+            const response = await axios.put(`${baseUrl}/issues/1`, updatedIssue);
+            expect(response.status).toBe(200);
+            expect(response.data).toEqual({
+                id: 1,
+                title: 'Updated Issue',
+                description: 'This is the updated issue'
+            });
+        });
+
+        it('should return 404 when the issue with the specified id does not exist', async () => {
+            try {
+                await axios.put(`${baseUrl}/issues/999`, { title: 'Update Title' });
+            } catch (error) {
+                expect(error.response.status).toBe(404);
+                expect(error.response.data).toEqual({ message: 'Issue not found' });
+            }
+        });
+    });
+
+    // DELETE /issues/:id
+    describe('DELETE /issues/:id', () => {
+        it('should delete the issue with the specified id', async () => {
+            const response = await axios.delete(`${baseUrl}/issues/2`);
+            expect(response.status).toBe(204);
+        });
+
+        it('should return 404 when the issue with the specified id does not exist', async () => {
+            try {
+                await axios.delete(`${baseUrl}/issues/999`);
+            } catch (error) {
+                expect(error.response.status).toBe(404);
+                expect(error.response.data).toEqual({ message: 'Issue not found' });
+            }
+        });
+    });
 });
